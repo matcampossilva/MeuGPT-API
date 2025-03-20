630f64b400fdd49ba56de048562b86bce2347f56
matcampossilva <mat.campos.silva@gmail.com>
2025-03-20 18:59:12 -0300

    Update enviar_whatsapp.py

--- enviar_whatsapp.py
+++ enviar_whatsapp.py
@@ -1,12 +1,12 @@
 # enviar_whatsapp.py
 from twilio.rest import Client
-from configuracoes import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN
+from configuracoes import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, MESSAGING_SERVICE_SID
 
-def enviar_whatsapp(mensagem, numero_destino="+5562999022021"):
+def enviar_whatsapp(mensagem, numero_destino="+5562992782150"):
     client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
     try:
         message = client.messages.create(
-            from_='whatsapp:+14155238886',
+            messaging_service_sid=MESSAGING_SERVICE_SID,  # 👈🏽 Aqui está o segredo!
             body=mensagem,
             to=f'whatsapp:{numero_destino}'
         )
@@ -14,6 +14,5 @@ def enviar_whatsapp(mensagem, numero_destino="+5562999022021"):
     except Exception as e:
         print(f"❌ Erro ao enviar WhatsApp: {e}")
 
-# Exemplo prático para testar imediatamente:
 if __name__ == "__main__":
-    enviar_whatsapp("Teste do Meu Conselheiro Financeiro!")
+    enviar_whatsapp("Teste do Meu Conselheiro Financeiro no número pessoal!")
