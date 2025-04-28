98b031208a4c33065f0c23cdfa693a13abd53ebf
Matheus <matheus@MacBook-Pro-3.local>
2025-04-28 12:31:13 -0300

    Correção do erro IndexError no histórico de conversa

--- planilhas.py
+++ planilhas.py
@@ -2,6 +2,8 @@ import os
 import gspread
 from dotenv import load_dotenv
 from oauth2client.service_account import ServiceAccountCredentials
+import datetime
+import pytz
 
 load_dotenv()
 
@@ -14,6 +16,9 @@ creds = ServiceAccountCredentials.from_json_keyfile_name(GOOGLE_SHEETS_KEY_FILE,
 gs = gspread.authorize(creds)
 
 _cache_abas = {}
+
+def formatar_numero(numero):
+    return numero.replace("whatsapp:", "").replace("+", "").replace(" ", "").strip()
 
 def get_aba(sheet_id, nome_aba):
     chave = f"{sheet_id}_{nome_aba}"
@@ -37,3 +42,20 @@ def get_limites():
 
 def get_gastos_fixos():
     return get_aba(GOOGLE_SHEET_GASTOS_ID, "Gastos Fixos")
+
+def get_user_sheet(user_number):
+    user_number = formatar_numero(user_number)
+    aba_pagantes = get_pagantes()
+    aba_gratuitos = get_gratuitos()
+
+    pagantes = [formatar_numero(num) for num in aba_pagantes.col_values(2)]
+    gratuitos = [formatar_numero(num) for num in aba_gratuitos.col_values(2)]
+
+    if user_number in pagantes:
+        return aba_pagantes
+    elif user_number in gratuitos:
+        return aba_gratuitos
+    else:
+        now = datetime.datetime.now(pytz.timezone("America/Sao_Paulo")).strftime("%d/%m/%Y %H:%M:%S")
+        aba_gratuitos.append_row(["", user_number, "", now, 0, 0])
+        return aba_gratuitos
