2a6138c81e4414656166d025986b394a288f0d2e
Matheus <matheus@MacBook-Pro-3.local>
2025-04-30 18:17:44 -0300

    Correção do erro IndexError no histórico de conversa

--- configuracoes.py
+++ configuracoes.py
@@ -9,5 +9,3 @@ WHATSAPP_FROM = 'whatsapp:+14155238886'
 
 EMAIL_REMETENTE = os.getenv('EMAIL_REMETENTE')
 SENHA_REMETENTE = os.getenv('SENHA_REMETENTE')
-
-GOOGLE_SHEETS_URL = 'https://script.google.com/macros/s/AKfycbyyChxdjQiNv8jmJEI--6vamvK6VSqWAZ0bh2gl0ky-vjsus0fYxjdQtHFj8vbHlSjP/exec'
